b525106b9a98b45d8c99caa5c6a1b4368c12f007
Ahmad Hassan <hassanahmed309@gmail.com>
2024-07-02 14:26:17 -0700

    Initial Setup, Devise and Navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    flash[:notice] = t("devise.sessions.signed_in")
+    root_path
+  end
+
+  def after_sign_out_path_for scope
+    flash[:notice] = t("devise.sessions.signed_out")
+    root_path
+  end
 end
